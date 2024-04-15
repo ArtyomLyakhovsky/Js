28ed652f3d8198c241526df035ce87d71bc06bd0
Artyom Lyakhovsky <lyaxovskij1995@gmail.com>
2024-04-15 16:27:03 +0300

    domashka

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const title = document.getElementById('title')
-const input = document.querySelector('input')
-const btn = document.querySelector('.btn');
 
-document.querySelector('.btn').onclick = function foo() {
-  let a = document.querySelector('input').value;
-  console.log(a);
-  document.getElementById('title').innerHTML = a;
-};
+// /////////Domashka//////////////////////////
+document.getElementById('btn').addEventListener('click', function() {
+  const inputText = document.getElementById('input').value;
+  const titleText = titleTextContent(inputText);
+  document.getElementById('title').textContent = titleText;
+});
 
-
-btn.addEventListener('click', function f() {
-  const title = document.getElementById('title')
-
-  let firstColor = Math.floor(Math.random() * (255 - 1 + 1)) +1;
-  let secondColor = Math.floor(Math.random() * (255 - 1 + 1)) +1;
-  let thirdColor = Math.floor(Math.random() * (255 - 1 + 1)) +1;
-
-  let rgb = 'rgb(' + firstColor + ', ' + secondColor + ', ' + thirdColor + ')';
-  title.style.color = rgb;
-})
+function titleTextContent(str) {
+  return str[0].toUpperCase() + str.toLowerCase().slice(1) 
+}
